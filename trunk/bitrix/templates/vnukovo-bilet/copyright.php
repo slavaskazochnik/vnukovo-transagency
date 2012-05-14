57b461686cb3c8e6106797ba45e0c8b5e517be39
klisin@gmail.com <klisin@gmail.com@abbed591-d0fb-934e-f481-d8110d6432d1>
2012-05-14 12:04:23 +0000

    Поменял местами год и название компании в копирайте

--- trunk/bitrix/templates/vnukovo-bilet/copyright.php
+++ trunk/bitrix/templates/vnukovo-bilet/copyright.php
@@ -4,7 +4,7 @@ IncludeTemplateLangFile(__FILE__);
 $START_YEAR = $START_YEAR > 0 ? $START_YEAR : 2005;
 ?>
 <div id="copyright">
-	<div class="company">&copy; <a href="<?= SITE_DIR ?>"><?=GetMessage('COPYRIGHT_COMPANY') ?></a> <?=$START_YEAR ?><?=date("Y") > $START_YEAR ? '&ndash;'.date("Y") : '' ?></div>
+	<div class="company">&copy; <?=$START_YEAR ?><?=date("Y") > $START_YEAR ? '&ndash;'.date("Y") : '' ?> <a href="<?= SITE_DIR ?>"><?=GetMessage('COPYRIGHT_COMPANY') ?></a></div>
 	<div class="reserved"><?=GetMessage('COPYRIGHT_RESERVED') ?></div>
 	<div class="policy"><a href="<?=SITE_DIR ?>privacy/"><?=GetMessage('COPYRIGHT_POLICY') ?></a></div>
 </div>
