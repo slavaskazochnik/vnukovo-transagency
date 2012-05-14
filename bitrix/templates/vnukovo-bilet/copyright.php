bf1dfd017534da7a7832998b35681d3884c8f5a8
klisin <klisin@gmail.com>
2012-05-14 10:41:27 +0000

    Корреция реплик и года в копирайте

--- bitrix/templates/vnukovo-bilet/copyright.php
+++ bitrix/templates/vnukovo-bilet/copyright.php
@@ -1,7 +1,7 @@
 <? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
-<? 
-IncludeTemplateLangFile(__FILE__); 
-$START_YEAR = $START_YEAR > 0 ? $START_YEAR : 2011;
+<?
+IncludeTemplateLangFile(__FILE__);
+$START_YEAR = $START_YEAR > 0 ? $START_YEAR : 2005;
 ?>
 <div id="copyright">
 	<div class="company">&copy; <a href="<?= SITE_DIR ?>"><?=GetMessage('COPYRIGHT_COMPANY') ?></a> <?=$START_YEAR ?><?=date("Y") > $START_YEAR ? '&ndash;'.date("Y") : '' ?></div>
--- bitrix/templates/vnukovo-bilet/lang/ru/copyright.php
+++ bitrix/templates/vnukovo-bilet/lang/ru/copyright.php
@@ -1,5 +1,5 @@
 <?
-$MESS ['COPYRIGHT_COMPANY'] = "Внуково-билет";
+$MESS ['COPYRIGHT_COMPANY'] = "Трансагентство Внуково";
 $MESS ['COPYRIGHT_RESERVED'] = "Все права защищены";
 $MESS ['COPYRIGHT_POLICY'] = "Политика защиты персональных данных";
 ?>
