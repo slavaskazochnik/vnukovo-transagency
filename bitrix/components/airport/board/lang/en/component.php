723730b8461b80e083148796e23c6d76c51a059b
klisin <klisin@gmail.com>
2012-06-01 10:14:38 +0000

    6:	В случае возврата пустого списка рейсов выводится соответствующая ошибка.

--- bitrix/components/airport/board/lang/en/component.php
+++ bitrix/components/airport/board/lang/en/component.php
@@ -5,6 +5,7 @@ $MESS ['AIRPORT_BOARD_NAME_PULKOVO'] = "Pulkovo";
 $MESS ['AIRPORT_BOARD_NAME_SHEREMETEVO'] = "Sheremetevo";
 $MESS ['AIRPORT_BOARD_CITY_MOSCOW'] = "Moscow";
 $MESS ['AIRPORT_BOARD_CITY_PETRESBURG'] = "Saint-Petersburg";
+$MESS ['AIRPORT_BOARD_NO_FLIGHTS'] = "#AIRPORT# scoreboard currently unavailable.<br />Please try again later.";
 $MESS ['AIRPORT_BOARD_STATUS_P'] = "On Time";
 $MESS ['AIRPORT_BOARD_STATUS_F'] = "Departed";
 $MESS ['AIRPORT_BOARD_STATUS_L'] = "Landed";
