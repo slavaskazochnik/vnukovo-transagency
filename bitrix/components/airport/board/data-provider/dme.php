7d7f10bff7c534adbc2b2e0674bcd9157563492f
klisin <klisin@gmail.com>
2012-06-02 22:04:55 +0000

    6:	Заработало табло Домодедово. Табло собирается за 3 запроса (сейчас третий запрос отключен, т.к. на хостинге не хватает памяти 44 МБ).

--- bitrix/components/airport/board/data-provider/dme.php
+++ bitrix/components/airport/board/data-provider/dme.php
@@ -13,25 +13,29 @@ class CAirportBoard
   {
     $result["INBOUND"] = Array(); // Список прилетающих рейсов
     $result["OUTBOUND"] = Array(); // Список вылетающих рейсов
-    $result["INBOUND"] = CAirportBoard::GetBoardFromSite( 'tabloname=TabloDeparture_R&d=1&v=19' );
-    $result["OUTBOUND"] = CAirportBoard::GetBoardFromSite( 'tabloname=TabloArrive_R&d=1&v=19' );
+    $result["INBOUND"] = CAirportBoard::GetBoardFromSite( 'tabloname=TabloDeparture_R&d=1' );
+    $result["OUTBOUND"] = CAirportBoard::GetBoardFromSite( 'tabloname=TabloArrive_R&d=1' );
     
     return $result;
   }
   
   function GetDateTimeArray ( $string ) // Разбор строки на дату и время
   {
-    preg_match_all( "/([0-9]{2}\:[0-9]{2})\s*([0-9]{2})\.([0-9]{2})/",
+    $month_search = array("янв", "фев", "мар", "апр", "мая", "июн", "июл", "авг", "сен", "окт", "ноя", "дек");
+    $month_replace = array("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
+    $string = str_replace($month_search, $month_replace, $string);
+    
+    preg_match_all( "/([0-9]{1,2})\s([0-9]{1,2})\s([0-9]{2}\:[0-9]{2})/",
       $string,
       $matches,
       PREG_PATTERN_ORDER
     );
     return Array(
         "DATE"      => Array(
-            "DAY"   => $matches[2][0],
-            "MONTH" => $matches[3][0],
+            "DAY"   => strlen($matches[1][0]) == 1 ? "0".$matches[1][0] : $matches[1][0],
+            "MONTH" => $matches[2][0],
           ),
-        "TIME"      => $matches[1][0]
+        "TIME"      => $matches[3][0]
       );
       
   }
@@ -40,7 +44,8 @@ class CAirportBoard
   {
     switch ( ToLower(trim($string)) )
     {
-      case "прилетел":
+      case "прибыл":
+      case "позднее прибытие":
         $result["CODE"] = "L";
         $result["NAME"] = GetMessage("AIRPORT_BOARD_STATUS_L");
       break;
@@ -75,157 +80,140 @@ class CAirportBoard
     return $result;
   }
   
-  function GetBoardFromSite ( $queryParameters ) // Загрузка и разбор табло с сайта
+  function GetOneBoardFromSite ( $queryParameters, $addQueryParameters )
   {
     global $APPLICATION;
     
-    $result = Array();
-  
     $ob = new CHTTP();
     $ob->http_timeout = 60;
     $ob->Query(
         "GET",
         "www.domodedovo.ru",
         80,
-        "/onlinetablo/?".$queryParameters.'&ts='.mktime(),
+        "/onlinetablo/?".$queryParameters.'&'.$addQueryParameters.'&ts='.mktime(),
         false,
         "",
         "N"
       );
+      
+    $res = $APPLICATION->ConvertCharset($ob->result, "UTF-8", SITE_CHARSET);
+      
+    $res = str_replace('<table cellspacing="1" cellpadding="3" width="100%" border="0" id="onlinetablo" >', "++++", $res);
+    $res = str_replace('</form>', "++++", $res);
+    $explode = explode("++++", $res);
+    $res = $explode[1];
+    $res = str_replace('</table>', "++++", $res);
+    $explode = explode("++++", $res);
+    $res = $explode[0];
+      
+    return Array(
+      "ERROR"      => Array(
+        "CODE"     => $ob->errno,
+        "MESSAGE"  => $ob->errstr
+        ),
+      "HTML"    => $res
+      );
+    unset($explode, $res);
+  }
   
-    $result["ERROR"]["CODE"] = $ob->errno;
-    $result["ERROR"]["MESSAGE"] = $ob->errstr; 
+  function GetBoardFromSite ( $queryParameters ) // Загрузка и разбор табло с сайта
+  {
+    $result = Array();
+    
+    $result = CAirportBoard::GetOneBoardFromSite( $queryParameters, "v=3" );
+    $res = $result["HTML"];
+    unset($result["HTML"]);
+    $result2 = CAirportBoard::GetOneBoardFromSite( $queryParameters, "v=11" );
+    $res .= $result2["HTML"];
+    unset($result2["HTML"]);
+    //$result3 = CAirportBoard::GetOneBoardFromSite( $queryParameters, "v=19" );
+    $res .= $result3["HTML"];
+    unset($result3["HTML"]);
+    
+    if ( intval($result2["ERROR"]["CODE"]) )
+    {
+      $result["ERROR"] = $result2["ERROR"];
+    } elseif ( intval($result3["ERROR"]["CODE"]) )
+    {
+      $result["ERROR"] = $result3["ERROR"];
+    }
     
     if ( !intval($result["ERROR"]["CODE"]) ) // Если данные были получены без ошибки
     {
-      $res = $APPLICATION->ConvertCharset($ob->result, "UTF-8", SITE_CHARSET);
+      
+      $res = '<table>'.$res.'</table>';
+      $res = str_replace("<br>", " ", $res);
+      $res = str_replace("<nobr>", "", $res);
+      $res = str_replace("</nobr>", "", $res);
       //trace($res);
       
-      $GrabStart = '<table cellspacing="1" cellpadding="3" width="100%" border="0" id="onlinetablo" >';
-      $GrabEnd = '</table>';
-      
-      $file = str_replace($GrabStart, "!!!", $file);
-      $file = str_replace($GrabEnd, "!!!", $file);
-      $file = str_replace("';this.bgColor='#E7F5FA';\" onmouseout=\"window.status='';this.bgColor='#ffffff';return;\"", "</td><td>1</td", $file);
-      $explode = explode("!!!", $file);
-      $file = $explode[1];
-      
-      $file = str_replace("onmouseover=\"window.status='", "><td>", $file);
-
-      $file = str_replace("<td", "::<td", $file);
-      $file = str_replace("<tr", "\n<tr", str_replace("\n", "", str_replace("\r", "", $file)));
-      
-      $file = str_replace("&quot;", "", $file);
-      $file = strip_tags($file);
-      
-      $explode = explode("\n", $file);
-      
-      foreach ( $explode as $line_num => $line_text )
-      {
-        $line_explode = explode("::", $line_text);
-        if ( $line_explode[2] == "1" || $line_explode[2] == "2" )
-        {
-          $temp = explode("(", $line_explode[1]);
-          $line_explode[1] = $temp[0];
-    
-          $temp = explode("(", $line_explode[2]);
-          $line_explode[2] = $temp[0];
-    
-          $temp = explode("(", $line_explode[3]);
-          $line_explode[3] = $temp[0];
-    
-          $temp = explode("(", $line_explode[4]);
-          $line_explode[4] = $temp[0];
-    
-          $type = str_replace("Идет регистрация", "регистр.", trim($line_explode[8]));
-          $type = str_replace("Посадка завершена", "на взлёте", $type);
-          $type = str_replace("Идет посадка", "посадка", $type);
-          
-          $type_test = explode(" ", $type);
-          if ( $type_test[2] == "на" )
-          {
-            $type = "регистр.";
-            $line_explode[10] = $type_test[3];
-          } else {
-            $line_explode[10] = "";
-          }
-          
-          $time_rasp = trim($line_explode[5]);
-          $time_wait = trim($line_explode[6]);
-            
-          $month_repl = array("янв" => "01", "фев" => "02", "мар" => "03", "апр" => "04", "мая" => "05", "июн" => "06", "июл" => "07", "авг" => "08", "сен" => "09", "окт" => "10", "ноя" => "11", "дек" => "12");
-      
-          foreach ( $month_repl as $a => $b )
-          {
-            $time_rasp = str_replace($a, $b, $time_rasp);
-            $time_wait = str_replace($a, $b, $time_wait);
-          }
-      
-          $time_rasp = explode(" ", $time_rasp);
-          $time_rasp = strtotime(date("Y")."-".$time_rasp[1]."-".$time_rasp[0]." ".$time_rasp[2].":00");
-          $time_wait = explode(" ", $time_wait);
-          $time_wait = strtotime(date("Y")."-".$time_wait[1]."-".$time_wait[0]." ".$time_wait[2].":00");
-      
-          $time = $time_rasp;
-      
-          $count++;
-      
-          $array_cells[$time][$count]['reis']				= trim(str_replace(" ", "", $line_explode[3]));
-          $array_cells[$time][$count]['aviacomp']			= trim($line_explode[1]);
-          $array_cells[$time][$count]['airport_from']		= trim($line_explode[4]);
-          $array_cells[$time][$count]['airport_to']		= trim($line_explode[4]);
-          $array_cells[$time][$count]['status']			= $type;
-          $array_cells[$time][$count]['time_plane']		= $time_rasp;
-          $array_cells[$time][$count]['time_wait']		= $time_wait;
-      
-          $array_cells[$time][$count]['terminal']			= trim($line_explode[10]);
-          $array_cells[$time][$count]['others']			= $row_array['cell'][9];
-          if ( $line_explode[2] == "1" )
-          {
-            $array_cells[$time][$count]['direction']		= "in";
-          } else {
-            $array_cells[$time][$count]['direction']		= "out";
-          }
-        }
-      }
-      
-      trace($array_cells);
-      
-      /*
       $xml = new CDataXML();
       if ( $xml->LoadString($res) )
       {
-        $node = $xml->SelectNodes("html");
-        trace($node->GetArray());
-        $rows = $node->elementsByName("row");
+        $node = $xml->SelectNodes("/table");
+        $rows = $node->elementsByName("tr");
+        $akNames = Array();
+        $akCodes = Array();
+        $departures = Array();
+        $arrivals = Array();
+        $terminals = Array();
         foreach ( $rows as $row )
         {
-          $cells = $row->elementsByName("cell");
-          // Определяем код авиакомпании и номер рейса
-          preg_match_all( "/([A-Za-zА-Яа-я0-9]{2})([0-9]+)/",
-              $cells[0]->content,
-              $flightNumber,
-              PREG_PATTERN_ORDER
-            );
-          $result["FLIGHTS"][] = Array(
-              "FLIGHT"            => Array(
-                  "AK_CODE"       => $flightNumber[1][0],
-                  "NUMBER"        => $flightNumber[2][0]
-                ),
-              "AK_NAME"           => htmlspecialchars( $cells[1]->content ),
-              "DEPARTURE"         => htmlspecialchars( $cells[2]->content ),
-              "ARRIVAL"           => htmlspecialchars( $cells[3]->content ),
-              "STATUS"            => CAirportBoard::GetStatusInfo( $cells[4]->content ),
-              "TIME"              => Array(
-                  "PLANNED"       => CAirportBoard::GetDateTimeArray( $cells[5]->content ),
-                  "ESTIMATED"     => CAirportBoard::GetDateTimeArray( $cells[6]->content ),
-                  "ACTUAL"        => CAirportBoard::GetDateTimeArray( $cells[7]->content )
-                ),
-              "TERMINAL"          => htmlspecialchars( $cells[8]->content )
-            );
+          if ( strstr($row->getAttribute("class"), "tr0") )
+          {
+            $cells = $row->elementsByName("td");
+            // Определяем код авиакомпании и номер рейса
+            preg_match_all( "/([A-Za-zА-Яа-я0-9]{2})\s*([0-9]+)\s*/",
+                $cells[0]->content,
+                $flightNumber,
+                PREG_PATTERN_ORDER
+              );
+            $result["FLIGHTS"][] = Array(
+                "FLIGHT"            => Array(
+                    "AK_CODE"       => $flightNumber[1][0],
+                    "NUMBER"        => $flightNumber[2][0]
+                  ),
+                "AK_NAME"           => "",
+                "DEPARTURE"         => htmlspecialchars( /*preg_replace("/^([^(]+)(\([а-яА-Я]+\)\s*)\s*$/", "${1} ${2}", $cells[1]->content)*/ $cells[1]->content ),
+                "ARRIVAL"           => htmlspecialchars( /*preg_replace("/^([^(]+)(\([а-яА-Я]+\)\s*)\s*$/", "${1} ${2}", $cells[1]->content)*/ $cells[1]->content ),
+                "STATUS"            => CAirportBoard::GetStatusInfo( $cells[5]->content ),
+                "TIME"              => Array(
+                    "PLANNED"       => CAirportBoard::GetDateTimeArray( $cells[2]->content ),
+                    "ESTIMATED"     => "",
+                    "ACTUAL"        => CAirportBoard::GetDateTimeArray( $cells[3]->content )
+                  ),
+                "TERMINAL"          => ""
+              );
+            // Формируем список уникальных терминалов и пунктов вылета и прилета для фильтра
+            if ( !in_array( $flightNumber[1][0], $akCodes) )
+            {
+              $akCodes[] = $flightNumber[1][0];
+            }
+            if ( !in_array(htmlspecialchars( $city[0]->content ), $departures) )
+            {
+              $departures[] = htmlspecialchars( $city[0]->content );
+            }
+            if ( !in_array(htmlspecialchars( $city[0]->content ), $arrivals) )
+            {
+              $arrivals[] = htmlspecialchars( $city[0]->content );
+            }
+            if ( !in_array(htmlspecialchars( $terminal[0]->content ), $terminals) )
+            {
+              $terminals[] = htmlspecialchars( $terminal[0]->content );
+            }
+          }
         }
+        sort($akNames);
+        sort($akCodes);
+        sort($departures);
+        sort($arrivals);
+        sort($terminals);
+        $result["AK"] = $akNames;
+        $result["AK_CODES"] = $akCodes;
+        $result["DEPARTURES"] = $departures;
+        $result["ARRIVALS"] = $arrivals;
+        $result["TERMINALS"] = $terminals;
       }
-      */
     }
     return $result;
   }
