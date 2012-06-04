bb5c43645f34f81f365152169e89e3b6e8efbb93
the.cat.2k@gmail.com <the.cat.2k@gmail.com@abbed591-d0fb-934e-f481-d8110d6432d1>
2012-06-04 12:15:23 +0000

    1: 	Шапка и подвал сайта

--- trunk/bitrix/templates/vnukovo-bilet/footer.php
+++ trunk/bitrix/templates/vnukovo-bilet/footer.php
@@ -3,9 +3,9 @@
 <? IncludeTemplateLangFile(__FILE__); ?>
 			<?
 			$pageTitle = $APPLICATION->GetTitle() != '' ? '<h1 class="page_title">' . $APPLICATION->GetTitle() . '</h1>' : ''; 		
-			$rightColClass = !$APPLICATION->GetPageProperty("HIDE_RIGHT_COLLUMN") && $APPLICATION->GetPageProperty("HIDE_TEXT_FIELD") != 'Y' ? 'sect_text' : '' ;
+			$rightColClass = !$APPLICATION->GetProperty("HIDE_RIGHT_COLLUMN") && $APPLICATION->GetProperty("HIDE_TEXT_FIELD") != 'Y' ? 'sect_text' : '' ;
 			if ( 
-				!$APPLICATION->GetPageProperty("HIDE_RIGHT_COLLUMN") &&
+				!$APPLICATION->GetProperty("HIDE_RIGHT_COLLUMN") &&
 				(file_exists($_SERVER["DOCUMENT_ROOT"].$APPLICATION->GetCurDir()."sect_system.php") && 
 				strlen($APPLICATION->GetFileContent($_SERVER["DOCUMENT_ROOT"].$APPLICATION->GetCurDir()."sect_system.php")) > 75 || 
 				file_exists($_SERVER["DOCUMENT_ROOT"].$APPLICATION->GetCurDir()."sect_right.php") && 
