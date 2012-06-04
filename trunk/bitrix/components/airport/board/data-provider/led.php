3f034b7bf34a30082aa96581257d499c2382a2cb
klisin@gmail.com <klisin@gmail.com@abbed591-d0fb-934e-f481-d8110d6432d1>
2012-06-04 21:14:20 +0000

    6:	Работа над Пулково. Осталось сделать предварительную сортировку рейсов.

--- trunk/bitrix/components/airport/board/data-provider/led.php
+++ trunk/bitrix/components/airport/board/data-provider/led.php
@@ -111,60 +111,39 @@ class CAirportBoard
       );
     unset($explode, $res);
   }
-
-  function GetBoardFromSite ( $queryParameters ) // Загрузка и разбор табло с сайта
-  {
-    $result = Array();
-
-    $result = CAirportBoard::GetOneBoardFromSite( $queryParameters, "p=1" );
-    $res = $result["HTML"];
-    unset($result["HTML"]);
-    $result2 = CAirportBoard::GetOneBoardFromSite( $queryParameters, "p=2" );
-    $res .= $result2["HTML"];
-    unset($result2["HTML"]);
-
-    if ( intval($result2["ERROR"]["CODE"]) )
+  
+  function AddFlightsToResult ( $html, $result, $terminal ) {
+  
+    $res = '<table>'.$html.'</table>';
+    $res = str_replace("&nbsp;", " ", $res);
+    $res = str_replace("<br>", " ", $res);
+    $res = preg_replace("/<!--.*-->/Uis", "", $res);
+    $res = preg_replace("/<colgroup>.*<\/colgroup>/Uis", "", $res);
+    $res = preg_replace("/<param[^>]*>/Uis", "", $res);
+    $res = preg_replace("/<a[^>]*>[^<]+<\/a>/Uis", "", $res);
+    
+    //trace($res);
+  
+    $xml = new CDataXML();
+    if ( $xml->LoadString($res) )
     {
-      $result["ERROR"] = $result2["ERROR"];
-    }
-
-    if ( !intval($result["ERROR"]["CODE"]) ) // Если данные были получены без ошибки
-    {
-
-      $res = '<table>'.$res.'</table>';
-      $res = str_replace("<br>", " ", $res);
-      $res = str_replace("<nobr>", "", $res);
-      $res = str_replace("</nobr>", "", $res);
-      $res = str_replace("&nbsp;", " ", $res);
-      $res = preg_replace("/<!--.*-->/Uis", "", $res);
-      $res = preg_replace("/<colgroup>.*<\/colgroup>/Uis", "", $res);
-      $res = preg_replace("/<a[^>]*>[^<]+<\/a>/Uis", "", $res);
-      //$res = preg_replace("/<param[^>]*>/Uis", "", $res);
-      trace($res);
-
-      $xml = new CDataXML();
-      if ( $xml->LoadString($res) )
-      {trace($xml->GetArray());
-        $node = $xml->SelectNodes("/table");
-        $rows = $node->elementsByName("tr");
-        $akNames = Array();
-        $akCodes = Array();
-        $departures = Array();
-        $arrivals = Array();
-        $terminals = Array();
-        $i = 0;
-        foreach ( $rows as $row )
+      $node = $xml->SelectNodes("/table");
+      $rows = $node->elementsByName("tr");
+      $i = 0;
+      foreach ( $rows as $row )
+      {//trace($row->getAttribute("class"));
+        if ( !strstr($row->getAttribute("class"), "onlineDetailTr") )
         {
-          if ( !strstr($row->getAttribute("class"), "onlineDetailTr") )
+          $cells = $row->elementsByName("td");
+          if ( count($cells) && preg_match("/([A-Za-zА-Яа-я0-9]{2})\s*([0-9]+)\s*/", $cells[0]->content) )
           {
-            $cells = $row->elementsByName("td");
             // Определяем код авиакомпании и номер рейса
             preg_match_all( "/([A-Za-zА-Яа-я0-9]{2})\s*([0-9]+)\s*/",
                 $cells[0]->content,
                 $flightNumber,
                 PREG_PATTERN_ORDER
               );
-
+  
             $result["FLIGHTS"][$i] = Array(
                 "FLIGHT"            => Array(
                     "AK_CODE"       => $flightNumber[1][0],
@@ -179,35 +158,72 @@ class CAirportBoard
                     "ESTIMATED"     => "",
                     "ACTUAL"        => CAirportBoard::GetDateTimeArray( $cells[3]->content )
                   ),
-                "TERMINAL"          => ""
+                "TERMINAL"          => $terminal
               );
             // Формируем список уникальных терминалов и пунктов вылета и прилета для фильтра
-            if ( !in_array( $flightNumber[1][0], $akCodes) )
+            if ( !in_array( $flightNumber[1][0], $result["AK_CODES"]) )
             {
-              $akCodes[] = $flightNumber[1][0];
+              $result["AK_CODES"][] = $flightNumber[1][0];
             }
-            if ( !in_array($result["FLIGHTS"][$i]["DEPARTURE"], $departures) )
+            if ( !in_array($result["FLIGHTS"][$i]["AK_NAME"], $result["AK_NAMES"]) && strlen($result["FLIGHTS"][$i]["AK_NAME"]) )
             {
-              $departures[] = $result["FLIGHTS"][$i]["DEPARTURE"];
+              $result["AK_NAMES"][] = $result["FLIGHTS"][$i]["AK_NAME"];
             }
-            if ( !in_array($result["FLIGHTS"][$i]["ARRIVAL"], $arrivals) )
+            if ( !in_array($result["FLIGHTS"][$i]["DEPARTURE"], $result["DEPARTURES"]) )
             {
-              $arrivals[] = $result["FLIGHTS"][$i]["ARRIVAL"];
+              $result["DEPARTURES"][] = $result["FLIGHTS"][$i]["DEPARTURE"];
+            }
+            if ( !in_array($result["FLIGHTS"][$i]["ARRIVAL"], $result["ARRIVALS"]) )
+            {
+              $result["ARRIVALS"][] = $result["FLIGHTS"][$i]["ARRIVAL"];
+            }
+            if ( !in_array($result["FLIGHTS"][$i]["TERMINAL"], $result["TERMINALS"]) )
+            {
+              $result["TERMINALS"][] = $result["FLIGHTS"][$i]["TERMINAL"];
             }
             $i++;
           }
         }
-        sort($akNames);
-        sort($akCodes);
-        sort($departures);
-        sort($arrivals);
-        sort($terminals);
-        $result["AK"] = $akNames;
-        $result["AK_CODES"] = $akCodes;
-        $result["DEPARTURES"] = $departures;
-        $result["ARRIVALS"] = $arrivals;
-        $result["TERMINALS"] = $terminals;
       }
+    }
+    unset($html, $xml, $res);
+    return $result;
+  }
+
+  function GetBoardFromSite ( $queryParameters ) // Загрузка и разбор табло с сайта
+  {
+    $result = Array();
+
+    $result = CAirportBoard::GetOneBoardFromSite( $queryParameters, "p=1" );
+    $result2 = CAirportBoard::GetOneBoardFromSite( $queryParameters, "p=2" );
+
+    if ( intval($result2["ERROR"]["CODE"]) )
+    {
+      $result["ERROR"] = $result2["ERROR"];
+    }
+
+    if ( !intval($result["ERROR"]["CODE"]) ) // Если данные были получены без ошибки
+    {
+      $result["AK_NAMES"] = Array();
+      $result["AK_CODES"] = Array();
+      $result["DEPARTURES"] = Array();
+      $result["ARRIVALS"] = Array();
+      $result["TERMINALS"] = Array();
+      
+      $result = CAirportBoard::AddFlightsToResult($result["HTML"], $result, "Пулково-1");
+      unset($result["HTML"]);
+      $result = CAirportBoard::AddFlightsToResult($result2["HTML"], $result, "Пулково-2");
+      unset($result2);
+      
+      sort($result["AK_NAMES"]);
+      sort($result["AK_CODES"]);
+      sort($result["DEPARTURES"]);
+      sort($result["ARRIVALS"]);
+      sort($result["TERMINALS"]);
+      
+      asort($result["FLIGHTS"]["TIME"]["PLANNED"]["DATETIME"]);
+      
+      //trace($result);
     }
     return $result;
   }
