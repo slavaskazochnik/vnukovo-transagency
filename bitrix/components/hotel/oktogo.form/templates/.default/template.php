b8cfad2c522abab0fd3d941d01bf7f816f89b656
the.cat.2k <the.cat.2k@gmail.com>
2012-07-02 10:29:53 +0000

    11: 	Краткая форма поиска гостиниц

--- bitrix/components/hotel/oktogo.form/templates/.default/template.php
+++ bitrix/components/hotel/oktogo.form/templates/.default/template.php
@@ -30,64 +30,49 @@ if ( !defined("__JQUERY_UI_CSS") ) {
   </style>
 <![endif]-->
 
-<div class="oktogo_search_form sect_text">
-	<form class="booking" method="get" id="frmSearch" name="frmSearch">
-		<h2><?= GetMessage("OKTOGO_HOTEL_SEARCH_FORM_HEADER") ?></h2>
-		<div class="line clearfix">
-			<div class="destination">
-				<input type="text" tabindex="1" placeholder="<?= GetMessage("OKTOGO_HOTEL_SEARCH_FORM_DESTINATION") ?>" id="dest" name="dest" maxlength="100" />
-			</div>
-			<input type="hidden" id="destination" name="destination"/>
-			<input type="hidden" id="destinationId" name="destinationId"/>
-			<input type="hidden" id="product" name="product" value="Hotel"/>
-			<input type="hidden" name="CheckInDate" id="CheckInDate" />
-			<input type="hidden" name="CheckOutDate"    id="CheckOutDate" />
-			<div class="date">
-				<input type="text" tabindex="2" placeholder="<?= GetMessage("OKTOGO_HOTEL_SEARCH_FORM_CHECKIN") ?>" id="CheckIn"/>
-			</div>
-			<div class="calendDelimetr">-</div>
-			<div class="date">
-				<input type="text" tabindex="3" placeholder="<?= GetMessage("OKTOGO_HOTEL_SEARCH_FORM_CHECKOUT") ?>" id="CheckOut"/>
-			</div>
-		</div>
-		
-		<div id="pnlSearchParams" class="line clearfix">
-			<div id="pnlRooms"></div>
-			<a href="javascript:;" title="<?= GetMessage("OKTOGO_HOTEL_SEARCH_FORM_ADD_ROOM") ?>" class="btnAddRoom"><?= GetMessage("OKTOGO_HOTEL_SEARCH_FORM_ADD_ROOM") ?></a>
-		</div>
-		<div>
-			<div class="validation-summary-valid" data-valmsg-summary="true" id="valSumSearch">
-				<ul><li style="display:none"></li></ul>
-			</div>
-		</div>
-		<div data-form-hidden="1" style="display:none"></div>
-		<input type="submit" class="booking_submit" value="<?= GetMessage("OKTOGO_HOTEL_SEARCH_FORM_BOOKING_SUBMIT") ?>" />
-		<div class="cb"></div>
-	</form>
+<div class="oktogo_search_form">
+  <form class="booking" method="get" id="frmSearch" name="frmSearch">
+      <div class="likeH1"><?= GetMessage("OKTOGO_HOTEL_SEARCH_FORM_HEADER") ?></div>
+      <div class="line">
+        <input type="text" tabindex="1" title="<?= GetMessage("OKTOGO_HOTEL_SEARCH_FORM_DESTINATION") ?>" id="dest" name="dest" maxlength="100" />
+        <input type="hidden" id="destination" name="destination"/>
+        <input type="hidden" id="destinationId" name="destinationId"/>
+        <input type="hidden" id="product" name="product" value="Hotel"/>
+        <input type="hidden" name="CheckInDate" id="CheckInDate" />
+        <input type="hidden" name="CheckOutDate"    id="CheckOutDate" />
+      </div>
+      <div class="calendLine">
+        <input type="text" tabindex="2" title="<?= GetMessage("OKTOGO_HOTEL_SEARCH_FORM_CHECKIN") ?>" id="CheckIn"/>
+        <div class="calendDelimetr">-</div>
+        <input type="text" tabindex="3" title="<?= GetMessage("OKTOGO_HOTEL_SEARCH_FORM_CHECKOUT") ?>" id="CheckOut"/>
+  
+      </div>
+      <div class="cb"></div>
+      <div id="pnlSearchParams">
+        <div id="pnlRooms"></div>
+        <a href="javascript:;" title="<?= GetMessage("OKTOGO_HOTEL_SEARCH_FORM_ADD_ROOM") ?>" class="btnAddRoom"><?= GetMessage("OKTOGO_HOTEL_SEARCH_FORM_ADD_ROOM") ?></a>
+      </div>
+      <div class="line">
+        <div class="validation-summary-valid" data-valmsg-summary="true" id="valSumSearch"><ul><li style="display:none"></li></ul></div>
+      </div>
+      <div data-form-hidden="1" style="display:none"></div>
+      <input type="submit" class="booking_submit" value="&nbsp;" title="<?= GetMessage("OKTOGO_HOTEL_SEARCH_FORM_BOOKING_SUBMIT") ?>" />
+          <!--[if lt IE 9]>
+            <em class="tl"></em>
+            <em class="tr"></em>
+          <![endif]-->
+          <em class="bottom">
+        <em class="bl"></em>
+        <em class="br"></em>
+      </em>
+  </form>
 </div>
+
 <script type="text/javascript">
 // <![CDATA[
-function changeLabels() {
-	$('#pnlRooms .numberLine').each(function(){
-		if ( $(this).children('.label').text() != '<?=GetMessage('OKTOGO_HOTEL_SEARCH_FORM_GUESTS')?>' ) {
-			$(this).children('.label').text('<?=GetMessage('OKTOGO_HOTEL_SEARCH_FORM_GUESTS')?>');
-		}
-		if ( $(this).children('.adults').text().indexOf('<?=GetMessage('OKTOGO_HOTEL_SEARCH_FORM_ADULT')?>') == -1 ){
-			$(this).children('.adults').prepend('<label><?=GetMessage('OKTOGO_HOTEL_SEARCH_FORM_ADULT')?></label>');
-		}
-		if ( $(this).children('.children').text().indexOf('<?=GetMessage('OKTOGO_HOTEL_SEARCH_FORM_CHILD')?>') == -1 ){
-			$(this).children('.children').prepend('<label><?=GetMessage('OKTOGO_HOTEL_SEARCH_FORM_CHILD')?></label>');
-		}
-	});
-}
-
 $().ready(function () {
   var api = new OKApi();
   api.searchForm();
-  
-  changeLabels();
 })
-
-$('#pnlSearchParams .btnAddRoom').click( function() {setTimeout(changeLabels, 20); });
 // ]]>
 </script>
