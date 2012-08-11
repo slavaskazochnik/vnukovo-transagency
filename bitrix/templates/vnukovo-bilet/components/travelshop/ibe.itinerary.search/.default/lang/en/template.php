02dfd0b129ccf2559e90bbf493c7cb8cebbea604
klisin <klisin@gmail.com>
2012-08-11 14:42:40 +0000

    В шаблон сайта добавлен шаблон компонента поиска МКВ.

---
+++ bitrix/templates/vnukovo-bilet/components/travelshop/ibe.itinerary.search/.default/lang/en/template.php
@@ -0,0 +1,10 @@
+<?
+$MESS['IBE_ITINERARY_SEARCH_TITLE'] = 'Itinerary receipt';
+$MESS['IBE_ITINERARY_SEARCH_ORDER_LABEL'] = 'Enter the order number';
+$MESS['IBE_ITINERARY_SEARCH_ORDER_PLACEHOLDER'] = 'digits';
+$MESS['IBE_ITINERARY_SEARCH_EMAIL_LABEL'] = 'and E-mail or phone buyer';
+$MESS['IBE_ITINERARY_SEARCH_EMAIL_PLACEHOLDER'] = 'specified when ordering';
+$MESS['IBE_ITINERARY_SEARCH_SUBMIT_LABEL'] = 'Download';
+$MESS['IBE_ITINERARY_SEARCH_IN_PROGRESS'] = 'The search for order';
+$MESS['IBE_ITINERARY_SEARCH_NOT_ACCESS'] = 'Server is not accessible';
+?>
