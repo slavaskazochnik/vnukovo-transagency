a128fbf3e46083f6c20a04af93759ed43c0a5188
klisin@gmail.com <klisin@gmail.com@abbed591-d0fb-934e-f481-d8110d6432d1>
2012-08-14 17:16:23 +0000

    В случае неоплаченного и/или отмененного заказа показывается соответствующая реплика об ошибке.

--- trunk/bitrix/components/travelshop/ibe.itinerary.search/lang/ru/component.php
+++ trunk/bitrix/components/travelshop/ibe.itinerary.search/lang/ru/component.php
@@ -1,6 +1,6 @@
 <?
 $MESS ['IBE_MODULE_NOT_INSTALL'] = "Модуль TravelShop не установлен.";
 $MESS ['IBE_ORDER_CANCELED'] = "Заказ отменен!";
-$MESS ['IBE_ORDER_NOT_PAYED'] = "Заказ не оплачен!";
+$MESS ['IBE_ORDER_NOT_PAID'] = "Заказ не оплачен!";
 $MESS ['IBE_ORDER_NOT_FOUND'] = "Заказ не найден!";
 ?>
