40ca28af5b245a247a38c49df14fc53c5f3d039c
klisin@gmail.com <klisin@gmail.com@abbed591-d0fb-934e-f481-d8110d6432d1>
2012-08-14 18:02:54 +0000

    В случае неоплаченного и/или отмененного заказа показывается соответствующая реплика об ошибке.

--- trunk/bitrix/components/travelshop/ibe.itinerary.search/lang/en/component.php
+++ trunk/bitrix/components/travelshop/ibe.itinerary.search/lang/en/component.php
@@ -1,7 +1,7 @@
 <?
 $MESS ['IBE_MODULE_NOT_INSTALL'] = "TravelShop module is not installed.";
 $MESS ['IBE_ORDER_CANCELED'] = "Order canceled!";
-$MESS ['IBE_ORDER_NOT_PAYED'] = "Order not payed!";
+$MESS ['IBE_ORDER_NOT_PAID'] = "Order not paid!";
 $MESS ['IBE_ORDER_NOT_FOUND'] = "Order not found!";
 $MESS ['IBE_ORDER_ERROR'] = "Error while search order";
 ?>
--- trunk/bitrix/components/travelshop/ibe.itinerary.search/lang/ru/component.php
+++ trunk/bitrix/components/travelshop/ibe.itinerary.search/lang/ru/component.php
@@ -1,7 +1,7 @@
 <?
 $MESS ['IBE_MODULE_NOT_INSTALL'] = "Модуль TravelShop не установлен.";
 $MESS ['IBE_ORDER_CANCELED'] = "Заказ отменен!";
-$MESS ['IBE_ORDER_NOT_PAYED'] = "Заказ не оплачен!";
+$MESS ['IBE_ORDER_NOT_PAID'] = "Заказ не оплачен!";
 $MESS ['IBE_ORDER_NOT_FOUND'] = "Заказ не найден!";
 $MESS ['IBE_ORDER_ERROR'] = "Ошибка при поиске заказа";
 ?>
